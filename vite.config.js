a8a625c45195915cf5510b1cd8d06ae7fb13b059
Dylan Moore <60218243+dymoo@users.noreply.github.com>
2021-04-14 23:09:47 +0100

    added auto deploy

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-/**
- * Babel will compile modern JavaScript down to a format compatible with older browsers, but it will also increase your
- * final bundle size and build speed. Edit the `browserslist` property in the package.json file to define which
- * browsers Babel should target.
- *
- * Browserslist documentation: https://github.com/browserslist/browserslist#browserslist-
- */
 const useBabel = true;
-
-/**
- * Change this to `true` to generate source maps alongside your production bundle. This is useful for debugging, but
- * will increase total bundle size and expose your source code.
- */
 const sourceMapsInProduction = false;
 
-/*********************************************************************************************************************/
-/**********                                              Vite                                               **********/
-/*********************************************************************************************************************/
-
 import { defineConfig } from 'vite';
-import path from 'path';
 import svelte from 'rollup-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 import legacy from '@vitejs/plugin-legacy';
@@ -34,8 +17,6 @@ const config = defineConfig({
       compilerOptions: {
         dev: !production,
       },
-
-      // @ts-ignore This is temporary until the type definitions are fixed!
       hot: !production,
     }),
   ],
@@ -53,7 +34,6 @@ const config = defineConfig({
   },
 });
 
-// Babel
 if (useBabel) {
   config.plugins.unshift(
     legacy({
@@ -61,29 +41,5 @@ if (useBabel) {
     })
   );
 }
-
-// Load path aliases from the tsconfig.json file
-// const aliases = tsconfig.compilerOptions.paths;
-
-// for (const alias in aliases) {
-//   const paths = aliases[alias].map((p) => path.resolve(__dirname, p));
-
-//   // Our tsconfig uses glob path formats, whereas webpack just wants directories
-//   // We'll need to transform the glob format into a format acceptable to webpack
-
-//   const wpAlias = alias.replace(/(\\|\/)\*$/, '');
-//   const wpPaths = paths.map((p) => p.replace(/(\\|\/)\*$/, ''));
-
-//   if (!config.resolve) config.resolve = {};
-//   if (!config.resolve.alias) config.resolve.alias = {};
-
-//   if (
-//     config.resolve &&
-//     config.resolve.alias &&
-//     !(wpAlias in config.resolve.alias)
-//   ) {
-//     config.resolve.alias[wpAlias] = wpPaths.length > 1 ? wpPaths : wpPaths[0];
-//   }
-// }
 
 export default config;
